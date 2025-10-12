733fbe0b5452479d1c51ed45007d0d71345ce558
halkolivan <tomayli@bk.ru>
2025-10-12 23:33:53 +0300

    fix

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./assets/styles/index.css"; // если нужно, или твой App.css
+import App from "@/App.jsx";
+import "@assets/styles/index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
