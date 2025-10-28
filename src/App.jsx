a5c7ccf7b39af4e3b57479b383bbbb983d92299a
halkolivan <tomayli@bk.ru>
2025-10-28 16:44:40 +0200

    set up styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./i18n";
 import Router from "./Router";
 import "@assets/styles/App.css";
+import "@assets/styles/fonts.css";
 import { registerSW } from "virtual:pwa-register";
 import React, { Suspense, useEffect } from "react";
 import { AuthProvider } from "@/context/AuthContext";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import Loader from "@assets/loaders/loaderBlack.svg";
-import "@assets/styles/fonts.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID ||
   "408629276793-90jf6aqt0lupftengqnodqd0dgnl2lck.apps.googleusercontent.com";
 
 function App() {
+  console.log("SW_CACHE_BUSTER_20251028_FINAL");
   useEffect(() => {
     if (import.meta.env.PROD) {
       registerSW({
