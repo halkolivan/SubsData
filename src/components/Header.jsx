f65ba5186e855df4df40397be404c75ffb85c844
halkolivan <tomayli@bk.ru>
2025-10-29 16:54:18 +0200

    fix server send mail

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -236,7 +236,7 @@ export default function Header() {
             <select
               value={i18n.language}
               onChange={(e) => changeLanguage(e.target.value)}
-              className="border rounded px-2 py-1 text-sm bg-gray-50/0 shadow-md shadow-sky-300 text-gray-700 
+              className="min-h-[48px] border rounded px-2 py-1 text-sm bg-gray-50/0 shadow-md shadow-sky-300 text-gray-700 
               hover:border-blue-50/0 border-blue-50/0 hover:shadow-green-400 cursor-pointer"
             >
               <option value="en">English</option>
