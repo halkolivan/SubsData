b20c4355ac090bd5a9c1c476bb7feeae8a0cffe2
halkolivan <tomayli@bk.ru>
2025-11-06 21:45:31 +0200

    fix index.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
-c
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   root: "src",
   base: "/",
