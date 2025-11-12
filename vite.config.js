3a44bfadbc29dc6728c3738fec826a7b511dbdc6
halkolivan <tomayli@bk.ru>
2025-11-12 20:49:54 +0200

    set fix

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
         orientation: "portrait",
         start_url: "/",
         scope: "/",
-        lang: "ru",
+        lang: "en",
         icons: [
           { src: "/icons/PWA192.png", sizes: "192x192", type: "image/png" },
           { src: "/icons/PWA512.png", sizes: "512x512", type: "image/png" },
