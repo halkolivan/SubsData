37e6a8a9cc476d2363847ccaa5754e764d89e24d
halkolivan <tomayli@bk.ru>
2025-11-12 13:45:07 +0200

    fix translate

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,11 @@
-import React from "react";
+import "./i18n";
 import App from "@/App.jsx";
 import "@assets/styles/index.css";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
 );
