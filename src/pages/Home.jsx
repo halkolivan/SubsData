3ea2ed286237eb4f87598ea3b5ff4fba68766d43
halkolivan <tomayli@bk.ru>
2025-11-15 19:02:05 +0200

    fix load home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import { formatDate, formatPrice } from "@/utils/formatUtils";
 
 export default function Home() {
   const { t } = useTranslation();
-  const { subscriptions, settings, isLoading } = useAuth();
+  const { subscriptions, settings, isLoading, user } = useAuth();
   const sourceSubs = subscriptions.length ? subscriptions : mockSubs;  
   const totalSubs = sourceSubs.length;
   const subSum = sourceSubs.reduce(
@@ -228,7 +228,16 @@ export default function Home() {
     }
   }, [subscriptions]);
 
-  
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen-main">
+        {/* Здесь должен быть виден текст, спиннер или ваш основной цвет */}
+        <h2 className="text-xl">
+          {user ? "Loading your subscriptions..." : "Loading..."}
+        </h2>
+      </div>
+    );
+  }
 
   return (
     <main className="flex h-auto w-full">
